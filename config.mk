ba6f4b5a3e0e140824ca967e0ccfae445b9ef7e4
Joe Maples <joe@frap129.org>
2016-08-29 22:02:00 +0000

    Remove SnapBrowser and BenzoBlock
    Signed-off-by: Joe Maples <joe@frap129.org>

--- config.mk
+++ config.mk
@@ -1,17 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-# Hide KA launcher
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kerneladiutor.hide=true
-
 # Substratum. Use recreate
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.substratum.recreate=true
 
 # Prebuilts
 PRODUCT_PACKAGES += \
-    BenzoBlock \
     KernelAdiutor \
-    SnapBrowser \
     Substratum
--- system/app/Android.mk
+++ system/app/Android.mk
@@ -1,31 +1,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# InstallBenzoBlock (smaller AdAway)
-include $(CLEAR_VARS)
-LOCAL_MODULE := BenzoBlock
-LOCAL_SRC_FILES := BenzoBlock.apk
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_JNI_LIBS_arm := \
-	@lib/armeabi/libblank_webserver_exec.so \
-	@lib/armeabi/libtcpdump_exec.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := SnapBrowser
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES    := SnapBrowser.apk
-LOCAL_CERTIFICATE  := PRESIGNED
-LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
-LOCAL_OVERRIDES_PACKAGES := Browser Browser2
-LOCAL_MULTILIB := 32
-include $(BUILD_PREBUILT)
-
 # Install Substratum
 include $(CLEAR_VARS)
 LOCAL_MODULE := Substratum
