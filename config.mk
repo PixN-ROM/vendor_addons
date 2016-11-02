d43acae6160227682d15451e15eb8b915b41f25b
mydongistiny <jaysonedson@gmail.com>
2016-11-01 18:26:50 -0700

    addons: Remove Substratum warning
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config.mk
+++ config.mk
@@ -1,6 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+# Remove warning from every time you open it
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.substratum.verified=true \
+
 # Prebuilts
 PRODUCT_PACKAGES += \
     Substratum \
